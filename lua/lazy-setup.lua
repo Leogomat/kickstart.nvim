df7115938f2f23a5dbab24a315c23029875c12b5
LeonardoGomesDaMatta <leonardo.gomes-da-matta-e-silva@deutschebahn.com>
2025-04-17 17:50:31 +0200

    update

---
+++ lua/lazy-setup.lua
@@ -0,0 +1,118 @@
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+-- [[ Configure and install plugins ]]
+require('lazy').setup({
+  -- Use `opts = {}` to force a plugin to be loaded.
+  -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
+  --
+  -- This is often very useful to both group configuration, as well as handle
+  -- lazy loading plugins that don't need to be loaded immediately at startup.
+  --
+  -- For example, in the following configuration, we use:
+  --  event = 'VimEnter'
+  --
+  -- which loads which-key before all the UI elements are loaded. Events can be
+  -- normal autocommands events (`:help autocmd-events`).
+  --
+  -- Then, because we use the `opts` key (recommended), the configuration runs
+  -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
+
+  -- NOTE: Plugins can specify dependencies.
+  --
+  -- The dependencies are proper plugin specifications as well - anything
+  -- you do for a plugin at the top level, you can do for a dependency.
+  --
+  -- Use the `dependencies` key to specify the dependencies of a particular plugin
+
+  -- LSP Plugins
+  require 'plugins.lspconfig',
+  require 'plugins.lazydev',
+  require 'plugins.luvit-meta',
+
+  'tpope/vim-surround',
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  require 'plugins.telescope',
+  require 'plugins.autocompletion',
+  require 'plugins.debug',
+  require 'plugins.indent-line',
+  require 'plugins.lint',
+  require 'plugins.autopairs',
+  require 'plugins.neo-tree',
+  require 'plugins.barbar',
+  require 'plugins.which-key',
+  require 'plugins.gitsigns',
+  require 'plugins.treesitter',
+  require 'plugins.sops',
+  require 'plugins.mini',
+  require 'plugins.todo-comments',
+  require 'plugins.colorscheme',
+  -- require 'plugins.window',
+  -- 'numToStr/Comment.nvim',
+  -- {
+  --   'numToStr/Comment.nvim',
+  --   config = function()
+  --     require('Comment').setup()
+  --   end,
+  -- },
+  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+
+  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+  --    This is the easiest way to modularize your config.
+  --
+  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+  -- { import = 'custom.plugins' },
+  --
+  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
+  -- Or use telescope!
+  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
+  -- you can continue same window with `<space>sr` which resumes last telescope search
+  -- {
+  --   {
+  --     'linux-cultist/venv-selector.nvim',
+  --     dependencies = {
+  --       'neovim/nvim-lspconfig',
+  --       'mfussenegger/nvim-dap',
+  --       'mfussenegger/nvim-dap-python', --optional
+  --       { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+  --     },
+  --     lazy = false,
+  --     branch = 'regexp', -- This is the regexp branch, use this for the new version
+  --     config = function()
+  --       require('venv-selector').setup()
+  --     end,
+  --     keys = {
+  --       { ',v', '<cmd>VenvSelect<cr>' },
+  --     },
+  --   },
+  -- },
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
